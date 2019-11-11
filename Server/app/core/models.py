86a787c3d946f987696f27773cde5344c76fe54d
aarajh <aarajh@gmail.com>
2019-11-11 00:27:59 -0600

    Created Custom User Models and Migrations

--- Server/app/core/models.py
+++ Server/app/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self,email, password=None, **extra_fields):
+        """Creates a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model supporting emails"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    """Assigns usermanager created above to the class"""
+    objects = UserManager()
+
+    USERNAME_FIELD= 'email'
