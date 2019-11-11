cc29f1d08eeeef6222ed9173abf90b981b3da567
aarajh <aarajh@gmail.com>
2019-11-11 00:39:51 -0600

    Added email normalization and empty check

--- Server/app/core/tests/tests_models.py
+++ Server/app/core/tests/tests_models.py
@@ -14,3 +14,16 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """Tests if the email is normalized"""
+        email = "test@DRAFTT.COM"
+        user = get_user_model().objects.create_user(email,"PasswordTest")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test if error is raised if no email is provided"""
+        email = ""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email,"123")
