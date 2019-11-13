d7053bf5cee5ae8d76402b10b5ccc4e9db418633
aarajh <aarajh@gmail.com>
2019-11-13 01:08:23 -0600

    Login endpoint created

--- Server/app/user/urls.py
+++ Server/app/user/urls.py
@@ -3,7 +3,8 @@ from user import views
 
 app_name = 'user'
 
+"""Redirects to the create user or login functions for each request"""
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
-    
+    path('login/', views.CreateTokenView.as_view(), name='login'),
 ]
