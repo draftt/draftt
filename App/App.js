496cf63764793be46f550814c92d6f7d6acf12d3
Adil Mian <adilmian95@gmail.com>
2019-11-20 06:56:20 -0500

    Populated stack navigator with initial routes

--- App/App.js
+++ App/App.js
@@ -1,12 +1,20 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen'
+import ScreenTester from './src/screens/ScreenTester';
 
 const navigator = createStackNavigator(
 	{
-		// Routes
+    // Routes
+    Home : HomeScreen,
+    Tester : ScreenTester
 	},
 	{
     // Stack navigator options
+    initialRouteName: "Tester",
+    defaultNavigationOptions: {
+      title: "draftt" // displayed on the top 
+    }
 	}
 );
 
