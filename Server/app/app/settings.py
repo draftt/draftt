0973530f7d25eb72be359b9fc66ebd68003e9186
aarajh <aarajh@gmail.com>
2019-11-23 17:09:49 -0600

    Fixed import error with social pipelines

--- Server/app/app/settings.py
+++ Server/app/app/settings.py
@@ -203,7 +203,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'app.load.load',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
