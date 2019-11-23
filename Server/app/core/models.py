82a70db49fdebfa1e75d3803d98a8fa7f3d0f4af
aarajh <aarajh@gmail.com>
2019-11-23 17:05:52 -0600

    Updated UserModel

--- Server/app/core/models.py
+++ Server/app/core/models.py
@@ -2,18 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, \
     BaseUserManager, PermissionsMixin
 from django.core.validators import validate_email
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger('app_api')
 
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, username, email, password="", **extra_fields):
+    def create_user(self,username, email,password=None,first_name="",**extra_fields):
         """Creates a new user"""
         if not username:
             raise ValueError('Users must have a username')
         if not email:
             raise ValueError('Users must have an email-address')
-        if not password:
-            raise ValueError('Users must have a password')
         validate_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
@@ -24,7 +27,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, username, email, password):
         """Creates and saves a new super user"""
 
-        user = self.create_user(username, email, password,
+        user = self.create_user(username, email, password=password,
                                 is_superuser=True, is_staff=True,
                                 is_verified=True)
         return user
@@ -34,7 +37,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     """User model supporting emails"""
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255, blank= True)
+    last_name = models.CharField(max_length=255, blank= True)
+    fullname = models.CharField(max_length=255, blank= True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
