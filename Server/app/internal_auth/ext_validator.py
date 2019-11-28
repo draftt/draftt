a4c2987996f154e00e54b06f6eec6ea01ac05e0e
aarajh <aarajh@gmail.com>
2019-11-28 04:44:58 -0600

    Extended OAuth-Toolkit Validators to include our grant type

---
+++ Server/app/internal_auth/ext_validator.py
@@ -0,0 +1,26 @@
+from oauth2_provider.oauth2_validators import OAuth2Validator
+from .models import ExtendedApplication 
+
+EXT_GRANT_TYPE_MAPPING = {
+    "authorization_code": (ExtendedApplication.GRANT_AUTHORIZATION_CODE, ),
+    "password": (ExtendedApplication.GRANT_PASSWORD, ),
+    "external_token": (ExtendedApplication.GRANT_EXTERNAL, ),
+    "client_credentials": (ExtendedApplication.GRANT_CLIENT_CREDENTIALS, ),
+    "refresh_token": (
+        ExtendedApplication.GRANT_AUTHORIZATION_CODE,
+        ExtendedApplication.GRANT_PASSWORD,
+        ExtendedApplication.GRANT_CLIENT_CREDENTIALS,
+    )
+}
+
+
+class ExtendedOAuth2Validator(OAuth2Validator):
+    """ Extension to the default OAuth2Validator 
+        to include external-token grant type
+    """
+    def validate_grant_type(self, client_id, grant_type, client, request, *args, **kwargs):
+        """
+        Validate both grant_type is a valid string and grant_type is allowed for current workflow
+        """
+        assert(grant_type in EXT_GRANT_TYPE_MAPPING)  # mapping misconfiguration
+        return request.client.allows_grant_type(*EXT_GRANT_TYPE_MAPPING[grant_type])
