21dd94b95e2c888fa56ee513aa6c77e711020e28
aarajh <aarajh@gmail.com>
2019-12-04 17:06:07 -0600

    PEP8

--- Server/app/core/management/commands/setup.py
+++ Server/app/core/management/commands/setup.py
@@ -1,6 +1,3 @@
-import time
-from django.db import connections
-from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 from app.settings import DEBUG
 from django.contrib.auth import get_user_model
@@ -8,21 +5,24 @@ from oauth2_provider.models import get_application_model
 
 Application = get_application_model()
 User = get_user_model()
+
+
 class Command(BaseCommand):
     """Django command to pause execution until db is available"""
+
     def createsuper(self):
         su = get_user_model().objects.create_superuser(
-                username="admin",
-                email="admin@admin.com",
-                password="admin"
-            )
+            username="admin",
+            email="admin@admin.com",
+            password="admin"
+        )
         return su
 
-    def createapp(self,superuser):
+    def createapp(self, superuser):
         form_data = {
             "name": "Test App",
             "client_id": "T001",
-            "user":superuser,
+            "user": superuser,
             "client_secret": "R2D2",
             "client_type": Application.CLIENT_CONFIDENTIAL,
             "redirect_uris": "http://localhost:8000",
@@ -33,16 +33,16 @@ class Command(BaseCommand):
         new_application.save()
         self.stdout.write(
             self.style.SUCCESS('client_id: T001')
-            )
+        )
         self.stdout.write(
             self.style.SUCCESS('client_secret: R2D2')
-            )
+        )
 
     def handle(self, *args, **options):
         if DEBUG is False:
             return
         self.stdout.write("Running in Debug Mode")
-        if Application.objects.count()>0 or User.objects.count()>0:
+        if Application.objects.count() > 0 or User.objects.count() > 0:
             self.stdout.write('Already test set up.')
             return
         su = self.createsuper()
