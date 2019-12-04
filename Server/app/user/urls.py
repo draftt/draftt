ed880884f6395fc13154675f9ec7405a3f0c3442
aarajh <aarajh@gmail.com>
2019-12-04 17:23:02 -0600

    Fixed authentication issue

--- Server/app/user/urls.py
+++ Server/app/user/urls.py
@@ -5,7 +5,7 @@ app_name = 'user'
 
 """Redirects to the create user or login functions for each request"""
 urlpatterns = [
-    # path('create/', views.CreateUserView.as_view(), name='create'),
+    path('create/', views.CreateUserView.as_view(), name='createuser'),
     # path('login/', views.CreateTokenView.as_view(), name='login'),
     path('account/', views.ManageUserView.as_view(), name='account'),
     # path('logout/', views.LogoutUser.as_view(), name='logout'),
