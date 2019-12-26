e109ded03644befe51a23ac9e69d2a7557716b1d
Aaraj Habib <aarajh@gmail.com>
2019-12-26 06:26:48 -0600

    Setup codecov and coverage.py

--- Server/app/app/settings.py
+++ Server/app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-try:
+try:            # pragma: no cover
     from .securekeys import *
 except ImportError:
     from .testkeys import *
