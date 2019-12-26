2993e1233512d9f7e9068b015c95beb73235f1bf
Aaraj Habib <aarajh@gmail.com>
2019-12-26 03:41:09 -0600

    Migrated latest django implementation supports timeout in seconds

--- Server/app/codegen/code_generator.py
+++ Server/app/codegen/code_generator.py
@@ -1,18 +1,56 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.crypto import salted_hmac
-from django.utils.http import int_to_base36
+from django.utils.crypto import constant_time_compare,salted_hmac
+from django.utils.http import int_to_base36, base36_to_int
 from .utils import truncate
 from django.conf import settings
+import logging
+from datetime import datetime
 
+log = logging.getLogger(__name__)
 class CodeGenerator(PasswordResetTokenGenerator):
     """
     Numeric small code generator based on Django
-    Password Reset Token Generator
+    Password Reset Token Generator that supports 
+    seconds timeout.
     """
 
     def __init__(self, code_type="default"):
-        super(CodeGenerator, self).__init__()
+        super().__init__()
         self.key_salt += code_type
+
+    def make_token(self, user):
+        """
+        Return a token that can be used once to do a password reset
+        for the given user.
+        """
+        return self._make_token_with_timestamp(user, self._num_seconds(self._now()))
+
+    def check_token(self, user, token):
+        """
+        Check that a password reset token is correct for a given user.
+        """
+        if not (user and token):
+            return False
+        # Parse the token
+        try:
+            ts_b36, _ = token.split("-")
+        except ValueError:
+            return False
+
+        try:
+            ts = base36_to_int(ts_b36)
+        except ValueError:
+            return False
+
+        # Check that the timestamp/uid has not been tampered with
+        if not constant_time_compare(self._make_token_with_timestamp(user, ts), token):
+            return False
+
+        # Check the timestamp is within limit.
+        if (self._num_seconds(self._now()) - ts) > settings.PASSWORD_RESET_TIMEOUT:
+            return False
+
+        return True
 
     def _make_token_with_timestamp(self, user, timestamp):
         ts_b36 = int_to_base36(timestamp)
@@ -24,3 +62,10 @@ class CodeGenerator(PasswordResetTokenGenerator):
         # Truncate the hash_string and ensure we have 6 characters
         hash_string = truncate(hash_string)[-settings.CODE_LENGTH:]
         return "%s-%s" % (ts_b36, hash_string)
+
+    def _num_seconds(self, dt):
+        return int((dt - datetime(2001, 1, 1)).total_seconds())
+
+    def _now(self):
+        # Used for mocking in tests
+        return datetime.now()
