f1e2627823b7ad0eb83396bbd95c33d5c8cf48f2
Aaraj Habib <aarajh@gmail.com>
2019-12-26 00:48:06 -0600

    Activation email test

--- Server/app/user/ tests/test_activation_api.py
+++ Server/app/user/ tests/test_activation_api.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
+from django.core import mail
 from rest_framework.test import APIClient
 from rest_framework import status
 
@@ -12,47 +12,38 @@ def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 
-class PublicUserApiTests(TestCase):
+class ActivationApiTests(TestCase):
     """Test the users API (public) that does not require authentication"""
 
     def setUp(self):
         self.client = APIClient()
-        payload = { 'username': 'testuser',
+        self.payload = { 'username': 'testuser',
                     'email': 'test@draftt.com', 
                     'password': 'testpass'
                     }
-        self.test_user= create_user(**payload)
+        self.res = self.client.post(CREATE_USER_URL, self.payload)
+        self.user =  get_user_model().objects.get(
+            username=self.payload['username'])
 
     def tearDown(self):
-        self.test_user.delete()
+        self.user.delete()
 
     def test_activation_data_returned_on_creation(self):
         """ Test if required activation data is returned on account
             creation"""
-        payload = {
-            'username': 'newuser',
-            'email': 'test@tester.com',
-            'password': 'testpass123',
-            'fullname': 'Test Name 123'
-        }
-        res = self.client.post(CREATE_USER_URL, payload)
-        self.assertIn('timestamp', res.data)
-        self.assertIn('uid', res.data)
+
+        self.assertIn('timestamp', self.res.data)
+        self.assertIn('uid', self.res.data)
 
     def test_new_user_inactive(self):
-        """ Test new user is inactive by default"""
-        payload = {
-            'username': 'newuser',
-            'email': 'test@tester.com',
-            'password': 'testpass123',
-            'fullname': 'Test Name 123'
-        }
-        self.client.post(CREATE_USER_URL, payload)
-
-        user = get_user_model().objects.get(username=payload['username'])
-        
-        self.assertFalse(user.is_active)
-
+        """ Test new user is inactive by default"""        
+        self.assertFalse(self.user.is_active)
     
+    def test_activation_email_send(self):
+        """ Test activation Email sent"""
+        self.assertTrue(len(mail.outbox) == 1)
+        self.assertIn("Activate", mail.outbox[0].subject)
 
-    
+    # def test_activation_code_expires(self):
+
+    # def test_activate_user(self):
