c2f1f058459a7a89981d31de0098c4da5e6491e4
Adil Mian <adilmian95@gmail.com>
2020-06-06 10:01:19 -0400

    Added a timeout to axios instance

--- pitch/src/Api/user.js
+++ pitch/src/Api/user.js
@@ -1,7 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 import Constants from "expo-constants";
-const {manifest} = Constants
+const { manifest } = Constants;
 
-export default axios.create({
-    baseURL : `http://${manifest.hostUri.split(':').shift()}:8080/api`    
-})
+const userApi = axios.create({
+	baseURL: `http://${manifest.hostUri.split(":").shift()}:8080/api`,
+});
+
+// set 500ms api call timeout
+userApi.defaults.timeout = 500;
+
+export default userApi;
