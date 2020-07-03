4c7fbcccd4036773d81ecb95333b14afeec4c809
Aaraj <aaraj@MacBook-Pro.local>
2020-07-03 15:42:35 -0500

    Added all screens to navigator

--- pitch/src/navigation.jsx
+++ pitch/src/navigation.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PublicStack from 'screens/Auth';
 import HomeScreen from 'screens/Home';
+import LeaguesScreen from 'screens/Leagues';
+import TeamScreen from 'screens/Team';
+import StatsScreen from 'screens/Stats';
+import SettingsScreen from 'screens/Settings';
 
 // Navigation stack declaration for routes only available when logged in
 function ProtectedStack() {
-  const Stack = createStackNavigator();
+  const Stack = createBottomTabNavigator();
   return (
     <Stack.Navigator
       initialRouteName="Home"
       headerMode="none"
     >
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Leagues" component={LeaguesScreen} />
+      <Stack.Screen name="Team" component={TeamScreen} />
+      <Stack.Screen name="Stats" component={StatsScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
 }
