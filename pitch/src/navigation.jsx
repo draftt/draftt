c9b3b987d1b802779a036ff59d9e744f81d85181
Aaraj <aaraj@MacBook-Pro.local>
2020-07-03 18:15:16 -0500

    Added Icons to nav bar

--- pitch/src/navigation.jsx
+++ pitch/src/navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PublicStack from 'screens/Auth';
@@ -8,6 +9,11 @@ import TeamScreen from 'screens/Team';
 import StatsScreen from 'screens/Stats';
 import SettingsScreen from 'screens/Settings';
 
+import { AntDesign } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+
 // Navigation stack declaration for routes only available when logged in
 function ProtectedStack() {
   const Stack = createBottomTabNavigator();
@@ -15,12 +21,70 @@ function ProtectedStack() {
     <Stack.Navigator
       initialRouteName="Home"
       headerMode="none"
+      tabBarOptions={{ showLabel: false, activeTintColor: '#fd7719' }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Leagues" component={LeaguesScreen} />
-      <Stack.Screen name="Team" component={TeamScreen} />
-      <Stack.Screen name="Stats" component={StatsScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Leagues"
+        component={LeaguesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <SimpleLineIcons name="trophy" size={size} color={color} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Team"
+        component={TeamScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <View style={{
+              height: size * 3,
+              width: size * 3,
+              backgroundColor: focused ? color : '#3B5998',
+              borderRadius: (size * 3) / 2,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              bottom: size / 2,
+
+            }}
+            >
+              <MaterialCommunityIcons
+                name="clipboard-text-outline"
+                size={size + 5}
+                color="white"
+              />
+            </View>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Stats"
+        component={StatsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="barschart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <EvilIcons name="gear" size={size} color={color} />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
