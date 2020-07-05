6a73b645c89d1ab6e4c2d5e90d3c38a35e7c52e6
Aaraj Habib <aarajh@gmail.com>
2020-07-05 03:48:04 -0500

    Renamed userInfo object

--- pitch/src/reducers/index.js
+++ pitch/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import pavilion from './pavilion';
 import auth from './auth';
-import userInfo from './user';
+import user from './user';
 
 export default combineReducers({
   auth,
   pavilion,
-  userInfo,
+  user,
 });
