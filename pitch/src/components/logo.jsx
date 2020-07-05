289224c03e40747e12a4421c3e59aa7d478c9317
Aaraj Habib <aarajh@gmail.com>
2020-07-05 01:18:28 -0500

    Logo component shows text logo if props given

--- pitch/src/components/logo.jsx
+++ pitch/src/components/logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import logoSrc from 'assets/nonamelogo/Logo_NoBG.png';
+import textLessLogo from 'assets/nonamelogo/Logo_NoBG.png';
+import textLogo from 'assets/logo/Logo_NoBG.png';
 import globalStyles from '../styles/styles';
 
-export default () => (
-  <View style={globalStyles.logoContainer}>
+export default ({ showText = false, style = {} }) => (
+  <View style={[globalStyles.logoContainer, style]}>
     <Image
-      source={logoSrc}
+      source={showText ? textLogo : textLessLogo}
       style={globalStyles.logo}
     />
   </View>
