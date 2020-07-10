6795fa50b2c8b5f14781b83b516b393566afffb4
Aaraj Habib <aarajh@gmail.com>
2020-07-09 20:20:29 -0500

    Added option for text only logo

--- pitch/src/components/logo.jsx
+++ pitch/src/components/logo.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import { View, Image } from 'react-native';
 import textLessLogo from 'assets/nonamelogo/Logo_NoBG.png';
 import textLogo from 'assets/logo/Logo_NoBG.png';
+import onlyTextLogo from 'assets/logo/Logo_onlyText.png';
 import globalStyles from '../styles/styles';
 
-export default ({ showText = false, style = {} }) => (
+export default ({ showText = false, onlyText = false, style = {} }) => (
   <View style={[globalStyles.logoContainer, style]}>
     <Image
-      source={showText ? textLogo : textLessLogo}
+      source={onlyText ? onlyTextLogo : (showText ? textLogo : textLessLogo ) }
       style={globalStyles.logo}
     />
   </View>
